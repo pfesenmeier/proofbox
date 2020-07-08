1534543b0e28f1c47b866ca94a6be0e8b0b7ebac
Paul Fesenmeier <pfesenmeier@gmail.com>
2020-07-07 21:21:35 -0400

    sensor code from example

--- sensor.py
+++ sensor.py
@@ -2,46 +2,57 @@ import board
 import busio
 import adafruit_sht31d
 
+from __future__ import division
+from webthing import(Action, Event, Property, SingleThing,
+                     Thing, Value, WebThingServer)
+
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_sht31d.SHT31D(i2c)
 
-print(f'Humidity: {sensor.relative_humidity}');
-print(f'Temperature: {sensor.temperature}');
+humidity = Value(sensor.relative_humidity)
+temperature = Value(sensor.temperature)
 
 iot = Thing(
-        'humid-temp-sht31d',
-        'SHT31D',
-        ['MultiLevelSensor','TemperatureSensor'],
-        'A web connected temperature and humidity sensor');
-level = Value(sensor.relative_humidity);
+    'humid-temp-sht31d',
+    'SHT31D',
+    ['MultiLevelSensor', 'TemperatureSensor'],
+    'A web connected temperature and humidity sensor')
+
 iot.add_property(
-        Property(
-            iot,
-            'level',
-            level,
-            metadata={
-                '@type': 'LevelProperty',
-                'title': 'Humidity',
-                'type': 'number',
-                'description': 'The current humidity in %',
-                'minimum': 0,
-                'maximum': 100,
-                'unit': 'percent',
-                'readOnly': True,
-                }
-            ))
-temperature = Value(sensor.temperature);
+    Property(
+        iot,
+        'level',
+        humidity,
+        metadata={
+            '@type': 'LevelProperty',
+            'title': 'Humidity',
+            'type': 'number',
+            'description': 'The current humidity in %',
+            'minimum': 0,
+            'maximum': 100,
+            'unit': 'percent',
+            'readOnly': True,
+        }
+    ))
+
 iot.add_property(
-        Property(
-            iot,
-            'temperature',
-            temperature,
-            metadata={
-                '@type': 'TemperatureProperty',
-                'title': 'Temperature',
-                'type': 'number',
-                'description': 'The current temperature in C',
-                'unit': 'degree celsius',
-                'readOnly': True,
-                }
-            ))
+    Property(
+        iot,
+        'temperature',
+        temperature,
+        metadata={
+            '@type': 'TemperatureProperty',
+            'title': 'Temperature',
+            'type': 'number',
+            'description': 'The current temperature in C',
+            'unit': 'degree celsius',
+            'readOnly': True,
+        }
+    ))
+
+server = WebThingServer(SingleThing(iot), port=8888)
+
+try:
+    server.start()
+except KeyboardInterrupt:
+    server.stop()

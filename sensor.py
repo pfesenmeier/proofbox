547458e067793a1fa1cabbe00554379311fe0e46
Paul Fesenmeier <pfesenmeier@gmail.com>
2020-07-08 19:39:34 -0400

    working sensor.py file

--- sensor.py
+++ sensor.py
@@ -1,58 +1,85 @@
-import board
-import busio
-import adafruit_sht31d
-
-from __future__ import division
+from __future__ import division, print_function
 from webthing import(Action, Event, Property, SingleThing,
                      Thing, Value, WebThingServer)
+import adafruit_sht31d
+import board
+import busio
+import time
+import tornado.ioloop
+import uuid
 
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_sht31d.SHT31D(i2c)
+class WeatherSensor:
+    def __init__(self):
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self.sensor = adafruit_sht31d.SHT31D(i2c)
+    def getTemp(self):
+        return self.sensor.temperature
+    def getHumidity(self):
+        return self.sensor.relative_humidity
 
-humidity = Value(sensor.relative_humidity)
-temperature = Value(sensor.temperature)
+class WeatherThing(Thing):
+    """a temperature and humidity sensor which updates every few seconds"""
 
-iot = Thing(
-    'humid-temp-sht31d',
-    'SHT31D',
-    ['MultiLevelSensor', 'TemperatureSensor'],
-    'A web connected temperature and humidity sensor')
+    def __init__(self, weather_sensor):
+        Thing.__init__(
+            self,
+            'humid-temp-sht31d',
+            'SHT31D',
+            ['MultiLevelSensor', 'TemperatureSensor'],
+            'A web connected temperature and humidity sensor'
+        )
+        self.humidity = Value(weather_sensor.getHumidity())
+        self.temperature = Value(weather_sensor.getTemp())
+        self.weather_sensor = weather_sensor
 
-iot.add_property(
-    Property(
-        iot,
-        'level',
-        humidity,
-        metadata={
-            '@type': 'LevelProperty',
-            'title': 'Humidity',
-            'type': 'number',
-            'description': 'The current humidity in %',
-            'minimum': 0,
-            'maximum': 100,
-            'unit': 'percent',
-            'readOnly': True,
-        }
-    ))
+        self.add_property(
+            Property(
+                self,
+                'level',
+                self.humidity,
+                metadata={
+                    '@type': 'LevelProperty',
+                    'title': 'Humidity',
+                    'type': 'number',
+                    'description': 'The current humidity in %',
+                    'minimum': 0,
+                    'maximum': 100,
+                    'unit': 'percent',
+                    'readOnly': True,
+                }))
 
-iot.add_property(
-    Property(
-        iot,
-        'temperature',
-        temperature,
-        metadata={
-            '@type': 'TemperatureProperty',
-            'title': 'Temperature',
-            'type': 'number',
-            'description': 'The current temperature in C',
-            'unit': 'degree celsius',
-            'readOnly': True,
-        }
-    ))
+        self.add_property(
+            Property(
+                self,
+                'temperature',
+                self.temperature,
+                metadata={
+                    '@type': 'TemperatureProperty',
+                    'title': 'Temperature',
+                    'type': 'number',
+                    'description': 'The current temperature in C',
+                    'unit': 'degree celsius',
+                    'readOnly': True,
+                }))
 
-server = WebThingServer(SingleThing(iot), port=8888)
+        self.timer = tornado.ioloop.PeriodicCallback(
+            self.update_weather,
+            3000
+        )
+        self.timer.start()
 
-try:
-    server.start()
-except KeyboardInterrupt:
-    server.stop()
+    def update_weather(self):
+        self.temperature.notify_of_external_update(self.weather_sensor.getTemp())
+        self.humidity.notify_of_external_update(self.weather_sensor.getHumidity())
+
+def run_server():
+    weather_thing = WeatherThing(WeatherSensor())
+    server = WebThingServer(SingleThing(weather_thing), port=8888)
+
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
+
+if __name__ == '__main__':
+    run_server()
